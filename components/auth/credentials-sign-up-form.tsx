19701408db3f9fbf94627ff6225f327b62140d35
Alejandro Pavon <alejandropm06@gmail.com>
2026-01-12 14:44:22 +0100

    Login

---
+++ components/auth/credentials-sign-up-form.tsx
@@ -0,0 +1,48 @@
+"use client";
+import { Label } from "@radix-ui/react-label";
+import React from "react";
+import { Input } from "../ui/input";
+import { Button } from "../ui/button";
+import { signUpDefaultValues } from "@/lib/constants";
+
+export default function CredentialsSignUpForm() {
+  return (
+    <form>
+      <div className="space-y-6">
+        <div>
+          <Label htmlFor="name">Name</Label>
+          <Input
+            id="name"
+            name="name"
+            type="text"
+            defaultValue={signUpDefaultValues.name}
+            required
+          />
+        </div>
+        <div>
+          <Label htmlFor="email">Email</Label>
+          <Input
+            id="email"
+            name="email"
+            type="text"
+            defaultValue={signUpDefaultValues.password}
+            required
+          />
+        </div>
+        <div>
+          <Label htmlFor="password">Password</Label>
+          <Input
+            id="password"
+            name="password"
+            type="text"
+            defaultValue={signUpDefaultValues.email}
+            required
+          />
+        </div>
+        <div>
+          <Button className="w-full">Sign Up</Button>
+        </div>
+      </div>
+    </form>
+  );
+}
--- lib/constants/index.ts
+++ lib/constants/index.ts
@@ -1,3 +1,11 @@
+import { email } from "zod"
+
 export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || "Tienda Next 2025"
 export const APP_DESCRIPTION = process.env.NEXT_PUBLIC_APP_DESCRIPTION || "Este es el proyecto Next de práctica en DAW"
 export const SERVER_URL = process.env.NEXT_PUBLIC_SERVER_URL || "https://localhost:3000"
+
+export const signUpDefaultValues ={
+    name: "alej",
+    email: "alejandropm06@gmail.com",
+    password:"a"
+}
